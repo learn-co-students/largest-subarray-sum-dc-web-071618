1882e1a59d45bc423a573dbd4132446f018be973
jensnyder <jlsnyder09@gmail.com>
2018-11-14 13:11:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-function largestSubarraySum(array) {
-  let largestSum = array[0];
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      for (let n = 1; n < array.length - 1; n++) {
-        let subArray = array.slice(i, j);
-        let sum = sumArray(subArray);
-        if (sum > largestSum) {
-          largestSum = sum;
-        }
-      }
-    }
-  }
-  return largestSum;
-}
+// function largestSubarraySum(array) {
+//   let largestSum = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       for (let n = 1; n < array.length - 1; n++) {
+//         let subArray = array.slice(i, j);
+//         let sum = sumArray(subArray);
+//         if (sum > largestSum) {
+//           largestSum = sum;
+//         }
+//       }
+//     }
+//   }
+//   return largestSum;
+// }
+//
+// function sumArray(array) {
+//   let sum = 0;
+//   for (let number of array) {
+//     sum += number;
+//   }
+//   return sum;
+// }
 
-function sumArray(array) {
-  let sum = 0;
-  for (let number of array) {
-    sum += number;
+function largestSubarraySum(a) {
+  var now = 0,
+    prev = 0;
+  for (var i = 0; i < a.length; i++) {
+    prev = Math.max(0, prev + a[i]);
+    now = Math.max(prev, now);
   }
-  return sum;
+  return now;
 }
